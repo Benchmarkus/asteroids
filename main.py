a0d4f14682a8c6ea8df6850548ef519728d90afe
Benchmarkus <toyryla.markus@gmail.com>
2025-05-31 13:46:49 +0300

    grouping spirits

--- main.py
+++ main.py
@@ -7,23 +7,31 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
         screen.fill(color=(0,0,0))
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for o in drawable:
+            o.draw(screen)
 
         dt = clock.tick(60) / 1000
         pygame.display.flip()
-
 
 
 
